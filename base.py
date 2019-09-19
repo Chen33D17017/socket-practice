a31aad4517e836c04ce2daf201954230f814d219
Chen33D17017 <s894117@gmail.com>
2019-09-19 23:06:44 +0900

    edit the content in base.py

--- base.py
+++ base.py
@@ -1,11 +1,30 @@
-from abc import ABC, abstractmethod
+import abc
+import socket
 
-class socket_base():
+class Socket_base(abc.ABC):
     def __init__(self, ip_address, port):
         self.ip_address = ip_address
         self.port = port
+
+    def __enter__(self):
+        self.s_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.init()
+        return self
+
+    @abc.abstractmethod
     def init(self):
-        
+        pass
+
+    @abc.abstractmethod
+    def send(self):
+        pass
+
+    @abc.abstractmethod
+    def receive(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.s_obj.close()
         
 if __name__ == '__main__':
     print('Hello World')
--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 import socket
+from base import Socket_base
+
+class Server(Socket_base):
+    def init(self):
+        pass
+
+    def build(self):
+        pass
+
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((socket.gethostname(), 1234))
-    s.listen(10)
+
     client_socket, address = s.accept()
     print(f"Connect to {address} has been established")
     client_socket.send("Hello World".encode("utf-8"))
